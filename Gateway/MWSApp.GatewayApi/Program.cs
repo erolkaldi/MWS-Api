d57c105c511205c56aa5f520ce44550048300386
erolkaldi <43037084+erolkaldi@users.noreply.github.com>
2022-09-30 11:40:28 +0300

    Ocelot Gateway

---
+++ Gateway/MWSApp.GatewayApi/Program.cs
@@ -0,0 +1,30 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddOcelot();
+builder.Configuration.AddJsonFile("configuration.json");
+
+builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        name: "AllowOrigin",
+        builder => {
+            builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+        });
+});
+var app = builder.Build();
+
+app.UseCors("AllowOrigin");
+app.UseOcelot();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
